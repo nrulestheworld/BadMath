e8da7ad08faeed22fd8286d654c86cf18a8a1a68
nrule <nrule@DESKTOP-IQ5VP1G>
2023-04-28 02:40:06 -0400

    fixesd

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -12,7 +12,7 @@ int main()
 	int n1 =0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
+	cout << "Second number: "; cin >> n2; 
 	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
